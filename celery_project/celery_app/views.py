41c54b62826accb126f19a02b2ce0e480b2b90db
dhiren parmar <dhiren.citrusbug@gmail.com>
2022-11-01 17:29:34 +0530

    1st Nov

--- celery_project/celery_app/views.py
+++ celery_project/celery_app/views.py
@@ -3,11 +3,14 @@ from django.views import View
 from celery_app.forms import LoginForm
 from django.contrib.auth import authenticate,login
 from . tasks import *
+from datetime import datetime, timedelta
+from pytz import timezone,utc
 # Create your views here.
 
 class Login(View):
     def get(self,request):
         form = LoginForm()
+        print(datetime.now(),'--',datetime.now()+timedelta(seconds=20))
         return render(request,'login.html',{'form':form})
     def post(self,request):
         form = LoginForm(request.POST)
@@ -25,8 +28,16 @@ class Login(View):
             user = authenticate(request,username=username, password=password)
             if user is not None:
                 # login_mail.apply_async(kwargs={'subject':'Subject here','message':'Here is the message.','from_email':'from@example.com','recipient_list':['to@example.com']})
-                login_mail.s('NEW Subject here','Here is the NEW message.','from@example.com',['to@example.com']).apply_async()
+                # login_mail.s('NEW Subject here','Here is the NEW message.','from@example.com',['to@example.com']).apply_async(countdown=20)
+                # local = timezone("America/Los_Angeles")
+                # naive = datetime.strptime("2001-2-3 10:11:12", "%Y-%m-%d %H:%M:%S")
+                # local_dt = local.localize(naive, is_dst=None)
+                # utc_dt = local_dt.astimezone(utc)
+                # utc_dt
+                # print(utc_dt.strftime("%Y-%m-%d %H:%M:%S"))
+                login_mail.apply_async(['NEW Subject here','Here is the NEW message.','from@example.com',['to@example.com']],eta=datetime.now()+timedelta(seconds=20))
                 login(request, user)
+                print(datetime.now(),'Time')
                 return redirect('/admin/')
             else:
                 print(form.cleaned_data)
